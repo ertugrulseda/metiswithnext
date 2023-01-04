b9ee826e53e10cddb9418cef9612e287ca3c41ea
Seda Ertuğrul <ertugrulseda@gmail.com>
2023-01-05 00:04:54 +0300

    mail send completed

--- components/emailform.js
+++ components/emailform.js
@@ -1,24 +1,45 @@
 import styles from '../styles/Emailform.module.css';
-
-const Emailform = ({ onClick }) => {
+import { useForm, ValidationError } from '@formspree/react';
+const Emailform = () => {
+	const [state, handleSubmit] = useForm("xknqqzqk"); //xknqqzqk
+	if (state.succeeded) {
+		return <p>Mailiniz İletildi En Kısa Sürede Dönüş Sağlayacağız !</p>;
+	}
 	return (
-		
-			<form className={styles.form}>
-				<input className={`${styles.textarea} ${styles.item1}`} type={'text'} placeholder="Ad Soyad" required />
-				<input className={`${styles.textarea} ${styles.item2}`} type={'email'} placeholder="Email" required />
-				<textarea
-					className={`${styles.textarea} ${styles.item3}`}
-					name="message"
-					rows="6"
-					id="inputMessage"
-					placeholder="Mesaj"
-					required=""
-				/>
-				<button className={ styles.item4} onClick={onClick}>
-					Mesaj Gönder
-				</button>
-			</form>
-	
+		<form className={styles.form} onSubmit={handleSubmit} >
+			<input id="adsoyad" className={`${styles.textarea} ${styles.item1}`} type={'text'} placeholder=" Ad Soyad" />
+			<ValidationError
+				prefix="adsoyad"
+				field="adsoyad"
+				errors={state.errors}
+			/>
+			<input
+				className={`${styles.textarea} ${styles.item2}`}
+				id="email"
+				type="email"
+				name="email"
+				placeholder=" Email"
+			/>
+			<ValidationError
+				prefix="Email"
+				field="email"
+				errors={state.errors}
+			/>
+			<textarea
+				className={`${styles.textarea} ${styles.item3}`}
+				id="message"
+				name="message"
+				placeholder=" Mesaj"
+			/>
+			<ValidationError
+				prefix="Message"
+				field="message"
+				errors={state.errors}
+			/>
+			<button className={styles.item4} type="submit" disabled={state.submitting}>
+				Mesaj Gönder
+			</button>
+		</form>
 	);
 };
 
--- pages/contact.js
+++ pages/contact.js
@@ -3,13 +3,10 @@ import Map from '../components/map';
 import Emailform from '../components/emailform';
 
 const Contact = () => {
-	const clickedSendMail = () => {
-		console.log('Mail Gönderildi');
-	};
 	return (
 		<div className={styles.contactcontainer}>
 			<Map />		
-			<Emailform onClick={clickedSendMail} />
+			<Emailform  />
 		</div>
 	);
 };
