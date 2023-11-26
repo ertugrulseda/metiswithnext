5f9c409b17e3eee9ae409af6fcb25067ee2f010b
Seda Ertuğrul <ertugrulseda@gmail.com>
2023-11-26 16:50:29 +0300

    Image kullanımları değiştirildi

--- pages/products.js
+++ pages/products.js
@@ -3,6 +3,7 @@ import ProductCard from '../components/productcard';
 import ImageLoader from '../components/imageloader';
 import Modal from '../components/modal';
 import styles from '../styles/Products.module.css';
+import Image from 'next/image';
 // import ContentLoader from 'react-content-loader';
 import { useState } from 'react';
 const productItemsQuery = `*[_type=="products" && isActive==true 
@@ -17,32 +18,61 @@ export default function Products({ productItems }) {
 	const [modalOpen, setModalOpen] = useState(false);
 	const [url, setUrl] = useState("");
 
-	const handleClick = (e) =>{
-		setModalOpen(true);
-		setUrl(e.target.src);
-		console.log(e.target.src);
+
+	const handleClick = (e) => {
+		const urlRegex = /url=([^&]+)/;
+		const inputString = e.target.src;
+		const match = inputString.match(urlRegex);
+		if (match) {
+			// Eğer eşleşme bulunduysa, URL'yi al
+			const foundURL = decodeURIComponent(match[1]);
+			console.log(foundURL);
+			setModalOpen(true);
+			setUrl(foundURL);
+			console.log(e.target.src);
+		} else {
+			console.log("URL bulunamadı");
+		}
+
 	}
 
 	return (
 		<div className={styles.productswrapper}>
-			{productItems.sort().map((item) => (
-				<ProductCard key={item._id}>
-					<Image src={urlFor(item.image).bg("ff00").url()} alt={item?.title} onClick={handleClick} />
-				</ProductCard>
-			))}
+			{
+
+				productItems.sort().map((item) => {
+					const url = urlFor(item.image).url();
+					const alt = item.icon;
+
+					return (
+						(url && alt) && <ProductCard key={item._id}>
+							<Image src={url} alt={alt} layout='fill' onClick={handleClick} />
+						</ProductCard>)
+
+				})
+			}
 			<Modal
 				shown={modalOpen}
 				close={() => {
 					setModalOpen(false);
 				}}
 			>
-				<Image src={url} />
+				<div className={styles.container} >
+					<div className={styles.div}>
+					<Image src={url} width={"400px"} height={"400px"} alt='dhfhf' />
+					</div>
+					
+					<div  className={styles.div} >
+						<h1 style={{textAlign:"center" }}>Açıklama</h1>
+						<p style={{textAlign:"center" }}>Deneme</p>
+					</div>
+				</div>
 			</Modal>
 		</div>
 	);
 }
 
-const Image = ({ src, alt, onClick }) => {
+/* const MyImage = ({ src, alt, onClick }) => {
 	const [imageLoaded, setImageLoaded] = useState(false);
 	const handleOnLoad = () => {
 		setImageLoaded(true);
@@ -50,11 +80,11 @@ const Image = ({ src, alt, onClick }) => {
 
 	return (
 		<div style={{ position: 'relative', width: '100%', height: '100%' }}>
-			<img alt={alt} loading="lazy" src={src} className={`${styles.image} `} onLoad={handleOnLoad} onClick={onClick} />
+			<Image src={src} width={"250px"} height={"250px"} alt={alt} />
 			{!imageLoaded && <ImageLoader />}
 		</div>
 	);
-};
+}; */
 
 
 export async function getServerSideProps() {
